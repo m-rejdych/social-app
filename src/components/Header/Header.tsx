940179b11993b92a5bed5634e30bd7177e683a84
Michał Rejdych <rejdych.m.r@gmail.com>
2020-09-20 01:38:39 +0200

    Added search bar to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppBar,
   Toolbar,
   IconButton,
   Box,
-  Typography,
+  TextField,
   makeStyles,
   Button,
 } from '@material-ui/core';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   text: {
@@ -19,9 +20,21 @@ const useStyles = makeStyles((theme) => ({
     padding: 0,
     fontSize: 24,
   },
+  textField: {
+    flexGrow: 1,
+    margin: theme.spacing(0, 6),
+    maxWidth: 500,
+  },
+  input: {
+    borderRadius: 40,
+  },
+  inputElement: {
+    padding: theme.spacing(1.4, 1),
+  },
 }));
 
 const Header: React.FC = () => {
+  const [searchValue, setSearchValue] = useState('');
   const classes = useStyles();
 
   return (
@@ -29,6 +42,20 @@ const Header: React.FC = () => {
       <Box clone width="100%">
         <Toolbar>
           <Button className={classes.text}>Social App</Button>
+          <TextField
+            value={searchValue}
+            type="search"
+            placeholder="Search..."
+            onChange={(e) => setSearchValue(e.target.value)}
+            variant="outlined"
+            className={classes.textField}
+            InputProps={{
+              startAdornment: <SearchIcon />,
+              className: classes.input,
+              color: 'secondary',
+            }}
+            inputProps={{ className: classes.inputElement }}
+          />
           <Box ml="auto">
             <IconButton>
               <MailOutlineIcon fontSize="large" />
